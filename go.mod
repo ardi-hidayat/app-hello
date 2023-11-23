4af5f8f95c0c9ec4fda99b426042cda4db88ff14
Ardi <ardihidayat51@gmail.com>
2023-11-23 22:15:39 +0700

    get module hello

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/ardi-hidayat/app-hello
+
+go 1.21.3
+
+require github.com/ardi-hidayat/go-hello v1.0.0 // indirect
---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	gohello "github.com/ardi-hidayat/go-hello"
+)
+
+func main() {
+
+	fmt.Println(gohello.SayHello())
+
+}
